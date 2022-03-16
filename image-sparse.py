73883e91fb7f64c20791cc7cd49431539be178db
Yongqiang Gu <x_forever@126.com>
2022-03-17 04:28:42 +0800

    更新 sparse image 解析
    Signed-off-by: Yongqiang Gu <x_forever@126.com>

--- image-sparse.py
+++ image-sparse.py
@@ -1,8 +1,12 @@
+#!/usr/bin/env python3
+
 from construct import *
+from construct.lib import hexdump
 
 """
-SPARSE IMAGE FORMAT: system/core/libsparse/sparse_format.h
- SPARSE IMAGE DEBUG: system/core/libsparse/simg_dump.py
+Sparse Image Format and Debug Tool (Android R):
+- system/core/libsparse/sparse_format.h
+- system/core/libsparse/simg_dump.py
 """
 
 SPARSE_HEADER = Struct(
@@ -35,7 +39,7 @@ CHUNK = Struct(
             {
                 "RAW": Bytes(this.header.value.total_size - CHUNK_HEADER.sizeof()),
                 "FILL": Int32ul,
-                "DONT_CARE": Byte[0],
+                "DONT_CARE": Bytes(0),
                 "CRC": Hex(Int32ul)
             }
         )
@@ -56,7 +60,7 @@ def pass_sparse_file(filename):
         sph = sparse.header.value
         print("*" * 40)
         print("HEADER:")
-        hexdump(sparse.header.data)
+        print(hexdump(sparse.header.data, 16))
         print("            magic: {}".format(sph.magic))
         print("    major version: {}".format(sph.major_version))
         print("    minor version: {}".format(sph.minor_version))
@@ -72,7 +76,7 @@ def pass_sparse_file(filename):
         i = 0
         for chunk in sparse.chunks:
             print("CHUNK {:d}:".format(i))
-            hexdump(chunk.header.data)
+            print(hexdump(chunk.header.data, 16))
             header = chunk.header.value
             print("  chunk type: {}".format(header.chunk_type))
             print("  chunk size: {}".format(header.chunk_size))
@@ -80,7 +84,7 @@ def pass_sparse_file(filename):
             print("")
             i += 1
 
-
+"""
 def hexdump(data, line_size=16, indent=0, prefix=""):
     for i in range(len(data)):
         if i % line_size == 0:
@@ -88,7 +92,7 @@ def hexdump(data, line_size=16, indent=0, prefix=""):
         print("{:02X} ".format(data[i]), end="")
         if i % line_size == (line_size - 1) or i == len(data) - 1:
             print("", end="\n")
-
+"""
 
 if __name__ == "__main__":
-    pass_sparse_file("cache.img")
+    pass_sparse_file("data/cache.img")
